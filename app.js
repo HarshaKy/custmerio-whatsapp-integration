6f0225d5eaa581ee29a421a9f55ea316db84ccf0
HarshaKy <harshaky6573@gmail.com>
2020-06-04 16:32:18 +0530

    tyntec whatsapp api

--- app.js
+++ app.js
@@ -1,110 +1,53 @@
 const CIO = require('customerio-node')
 const express = require('express')
-const twilio = require('twilio')
-const emailValidator = require('email-validator')
 const bodyParser = require('body-parser')
 
-const { MessagingResponse } = require('twilio').twiml
 const { siteId } = require('./keys')
 const { apiKey } = require('./keys')
-const { accountSid } = require('./keys')
-const { authToken } = require('./keys')
+
+const { handleCustomerioMessage } = require('./handleCustomerioMessage')
 
 const cio = new CIO(siteId, apiKey)
-const client = require('twilio')(accountSid, authToken);
 const app = express()
 
 app.use(bodyParser.json())
 
-// route to register through whatsapp
 app.post('/fromWhatsapp', (req, res) => {
     
-    const twiml = new MessagingResponse()
-
-    let messageBody
-
-    client.messages.list({limit: 1}).then((messages) => {
-        messageBody = messages[0].body
-        messageFrom = messages[0].from.replace('whatsapp:', '')
-        
-        if(!emailValidator.validate(messageBody)) {
-
-            twiml.message(`Hello ${messageFrom}. Enter email to register`)
-
-            res.writeHead(200, { 'Content-Type': 'text/xml' })
-            res.end(twiml.toString())
-        } else {
-
-            cio.identify(messageFrom, {
-                phone: messageFrom,
-                email: messageBody,
-                channel: 'WhatsApp',
-                plan: 'basic',
-                created_at: Math.floor( Date.now() / 1000 )
-            
-            })
-            .then(() => {
-                
-                twiml.message(`Awaiting confirmation...`)
-
-                res.writeHead(200, { 'Content-Type': 'text/xml' })
-                res.end(twiml.toString())
-            
-            }, (e) => {
-                console.log(e)
-            })
-        }   
+    messageFrom = '+' + req.body.from
+    
+    cio.identify(messageFrom, {
+        phone: messageFrom,
+        channel: 'WhatsApp',
+        created_at: Math.floor( Date.now() / 1000 )
+    }).then(() => {
+        res.send('ok').status(200)
+    }, (e) => {
+        console.log(e)
     })
 })
 
-// route to send welcome message from customerio to whatsapp
 app.post('/welcomeMessage', (req, res) => {
 
-    if (req.body.message) {
-        console.log(req.body.phone)
-        
-        client.messages
-            .create({
-                from: 'whatsapp:+14155238886',
-                body: req.body.message,
-                to: `whatsapp:${req.body.phone}`
-            })
-            .then(message => console.log(message.sid))
-        
-        res.send('ok').status(200)
-    }
+    handleCustomerioMessage(req)
+
+    res.send('ok').status(200)
+
 })
 
 app.post('/broadcast', (req, res) => {
-    if (req.body.message) {
-        console.log(req.body.phone)
-        
-        client.messages
-            .create({
-                from: 'whatsapp:+14155238886',
-                body: req.body.message,
-                to: `whatsapp:${req.body.phone}`
-            })
-            .then(message => console.log(message.sid))
-        
-        res.send('ok').status(200)
-    }
+    
+    handleCustomerioMessage(req)
+    
+    res.send('ok').status(200)
+
 })
 
 app.post('/whatsappNewsletter', (req, res) => {
     
-    if (req.body.news) {
-        
-        client.messages
-            .create({
-                from: 'whatsapp:+14155238886',
-                body: req.body.news,
-                to: `whatsapp:${req.body.phone}`
-            })
-            .then(message => console.log(message.sid))
-    
-        res.send('ok').status(200)
-    }
+    handleCustomerioMessage(req)
+
+    res.send('ok').status(200)
 
 })
 
---
+++ handleCustomerioMessage.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch')
+const { apikey } = require('./keys')
+const { fromPhoneNumber } = require('./phoneNumbers')
+
+const handleCustomerioMessage = (req) =>{
+    const inputBody = {
+        "to": req.body.phone,
+        "channels": [
+            "whatsapp"
+        ],
+        "whatsapp": {
+            "from": fromPhoneNumber,
+            "contentType": "text",
+            "text": req.body.message
+        }
+    }
+    
+    const headers = {
+        'Content-Type':'application/json',
+        'Accept':'application/json',
+        'apikey': apikey
+    }
+    
+    fetch('https://api.tyntec.com/chat-api/v2/messages', {
+        method: 'POST',
+        body: JSON.stringify(inputBody),
+        headers: headers
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((body) => {
+        console.log(body)
+    })
+}
+
+module.exports = { handleCustomerioMessage }
